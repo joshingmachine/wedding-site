b993d8804b68e866e5e57c61f50006696d07668c
joshingmachine <josh@joshingmachine.com>
2018-10-08 19:52:36 -0400

    Using dotenv for secret keeping

---
+++ index.js
@@ -0,0 +1,9 @@
+const envConfigPath =
+    // eslint-disable-next-line no-process-env
+    process.env.NODE_ENV === 'production' ? '.env' : '.env.public'
+
+require('dotenv').config({
+    path: envConfigPath,
+})
+
+console.log(process.env.TITLE)
