9eca84268eadb93ac2fdc73e58367f36e66b4eae
joshingmachine <josh@joshingmachine.com>
2018-10-08 23:17:37 -0400

    Parse html file and replace placeholders

--- src/index.js
+++ src/index.js
@@ -1,10 +1,33 @@
-const envConfigPath =
+const fs = require('fs')
+
+const configPath =
     // eslint-disable-next-line no-process-env
     process.env.NODE_ENV === 'production' ? '.env' : '.env.public'
 
-require('dotenv').config({
-    path: envConfigPath,
+const configResult = require('dotenv').config({
+    path: configPath,
 })
 
-console.log(process.env.TITLE)
-console.log(process.env.DATE_TIME)
+if (configResult.error) {
+    throw configResult.error
+}
+
+// eslint-disable-next-line no-sync
+const rawContent = fs.readFileSync('src/index.html', {
+    encoding: 'utf8',
+})
+
+const configVars = Object.keys(configResult.parsed)
+
+const replacePlaceholder = (fileData, configVar) => {
+    const placeholder = `\\$${configVar}`
+    return fileData.replace(
+        new RegExp(placeholder, 'g'),
+        // eslint-disable-next-line no-process-env
+        process.env[configVar]
+    )
+}
+
+const fileContent = configVars.reduce(replacePlaceholder, rawContent)
+
+console.log(fileContent)
