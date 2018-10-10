1029f96af2685c30ada002eb758c36de28fe4733
joshingmachine <josh@joshingmachine.com>
2018-10-09 23:28:07 -0400

    Use .env.default for default values, override with .env if present

--- src/index.js
+++ src/index.js
@@ -1,16 +1,28 @@
 const fs = require('fs')
+const dotenv = require('dotenv')
 const outputDir = 'dist'
 
-const configPath =
-    // eslint-disable-next-line no-process-env
-    process.env.NODE_ENV === 'production' ? '.env' : '.env.default'
-
-const configResult = require('dotenv').config({
-    path: configPath,
+const configResult = dotenv.config({
+    path: '.env.default',
 })
 
 if (configResult.error) {
     throw configResult.error
+}
+
+// eslint-disable-next-line no-process-env
+if (process.env.NODE_ENV === 'production') {
+    // eslint-disable-next-line no-sync
+    const prodConfig = dotenv.parse(fs.readFileSync('.env'))
+
+    for (const prodKey in prodConfig) {
+        if (Object.prototype.hasOwnProperty.call(prodConfig, prodKey)) {
+            if (prodConfig[prodKey]) {
+                // eslint-disable-next-line no-process-env, prefer-destructuring
+                process.env[prodKey] = prodConfig[prodKey]
+            }
+        }
+    }
 }
 
 const configVars = Object.keys(configResult.parsed)
